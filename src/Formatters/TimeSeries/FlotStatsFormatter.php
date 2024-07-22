1cd108c4ef611c556dc4038aec55eb021c107d0d
Arushad Ahmed <dash-8x@hotmail.com>
2024-07-22 13:11:36 +0500

    - Added flot stats formatter

---
+++ src/Formatters/TimeSeries/FlotStatsFormatter.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Chartjs stats formatter
+ */
+
+namespace Javaabu\Stats\Formatters\TimeSeries;
+
+use Javaabu\Stats\Contracts\TimeSeriesStatsRepository;
+use Javaabu\Stats\Enums\TimeSeriesModes;
+
+class FlotStatsFormatter extends AbstractTimeSeriesStatsFormatter
+{
+    /**
+     * Format the data
+     */
+    public function format(TimeSeriesModes $mode, TimeSeriesStatsRepository $stats, ?TimeSeriesStatsRepository $compare = null): array
+    {
+        $stats_repo = $stats;
+
+        $stats_interval = $stats_repo->interval($mode);
+        $max_interval = $stats_interval;
+
+        $field_name = $stats_repo->getAggregateFieldName();
+
+        $stats = $stats_repo->results($mode)->pluck($field_name, $mode->value);
+
+        // loop over all
+        $stat_next_date = $stats_repo->getDateFrom()->copy();
+
+        $stats_data = [];
+
+        for ($i = 0; $i <= $max_interval; $i++) {
+            // get the stat
+            $stat_date_str = $mode->formatDate($stat_next_date, false);
+            $stat = $stats->get($stat_date_str);
+            $stats_data[] = [$i, $stat ? $stat + 0 : 0];
+            $stat_next_date = $mode->increment($stat_next_date);
+        }
+
+        return $stats_data;
+    }
+}
---
+++ tests/Feature/Formatters/FlotStatsFormatterTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Javaabu\Stats\Tests\Feature\Formatters;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Javaabu\Stats\Enums\PresetDateRanges;
+use Javaabu\Stats\Enums\TimeSeriesModes;
+use Javaabu\Stats\Formatters\TimeSeries\ChartjsStatsFormatter;
+use Javaabu\Stats\Formatters\TimeSeries\DefaultStatsFormatter;
+use Javaabu\Stats\Formatters\TimeSeries\FlotStatsFormatter;
+use Javaabu\Stats\Tests\TestCase;
+use Javaabu\Stats\Tests\TestSupport\Factories\ActivityFactory;
+use Javaabu\Stats\Tests\TestSupport\Stats\TimeSeries\UserLogouts;
+
+class FlotStatsFormatterTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_can_format_with_the_flot_formatter(): void
+    {
+        $this->travelTo('2024-07-04');
+
+        // yesterday
+        ActivityFactory::new()
+            ->logout()
+            ->withUser()
+            ->count(5)
+            ->create([
+                'created_at' => '2024-07-03',
+            ]);
+
+        // today
+        ActivityFactory::new()
+            ->logout()
+            ->withUser()
+            ->count(2)
+            ->create([
+                'created_at' => '2024-07-04',
+            ]);
+
+        // create the stat
+        $stat = new UserLogouts(PresetDateRanges::LAST_7_DAYS);
+        $compare = new UserLogouts(PresetDateRanges::LAST_7_DAYS->getPreviousDateRange());
+
+        $formatter = new FlotStatsFormatter();
+
+        $data = $formatter->format(TimeSeriesModes::DAY, $stat, $compare);
+
+        $this->assertIsArray($data);
+
+        $this->assertEquals([
+            [0, 0],
+            [1, 0],
+            [2, 0],
+            [3, 0],
+            [4, 0],
+            [5, 5],
+            [6, 2],
+        ], $data);
+    }
+}
